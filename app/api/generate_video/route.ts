0cb24366a69b50094322248f6a0848a86054e9b3
aginamena <aginamena5@gmail.com>
2026-01-24 13:04:55 -0500

    removed commented prompt

--- app/api/generate_video/route.ts
+++ app/api/generate_video/route.ts
@@ -9,12 +9,7 @@ export async function POST(req: Request) {
     const prompt = formData.get("prompt") as string;
 
     const imageUrls = await uploadImages(files);
-
-// const prompt = `
-// these are new products that arrived today. create a high-energy video ad concept showcasing these products 
-// and telling people to visit our store, Toby Authentic Afro-Caribbean foods, at 1339 viking drive ottawa
-// or call us at 613-733-232
-// `
+    
 const design = await generateDesign(prompt, imageUrls)
 const designWithUrls = await AddVoiceoverURLsAndImageURLsToDesign(design, imageUrls)
 const remotionCode = await generateRemotionCode(designWithUrls)
