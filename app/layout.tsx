ed943e8097a6be9eee71a07408ab9902926d82bd
aginamena <aginamena5@gmail.com>
2026-01-25 04:10:25 -0500

    added mouseflow

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+ window._mfq = window._mfq || [];
+  (function() {
+    var mf = document.createElement("script");
+    mf.type = "text/javascript"; mf.defer = true;
+    mf.src = "//cdn.mouseflow.com/projects/286927fc-a8a6-428c-9906-89bb42df4d78.js";
+    document.getElementsByTagName("head")[0].appendChild(mf);
+  })();
+            `,
+          }}
+        />
+      </head>
       <body suppressHydrationWarning>
         <ThemeRegistry>
           <NavigationBar />
